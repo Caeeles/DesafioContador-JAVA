17d90c5529e4d02e13177b74e8ba6dcd5240cd0d
Caeeles <mv.carloseduardosilva@gmail.com>
2024-06-26 15:21:29 -0300

    Classe Contador e Classe ParametrosInvalidosException

---
+++ src/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package controlefluxo;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String message){
+        super(message);
+    }
+    
+}
